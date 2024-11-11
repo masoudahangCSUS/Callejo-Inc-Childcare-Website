9b7ca4395b09289fd2df3bd92025c81a7b323a9b
EvanCallejo <109547139+EvanCallejo@users.noreply.github.com>
2024-11-11 14:02:03 -0800

    Created foundations for RESTful API to connect database to backend

---
+++ Common/View/RoleView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.View
+{
+    public class RoleView
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+    }
+    public class ListRoles : APIResponse
+    {
+        public List<RoleView> roles { get; set; }
+
+        public ListRoles()
+        {
+            roles = new List<RoleView>();
+        }
+    }
+}
