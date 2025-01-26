4c1a558f9aad36804d87a99ca58b42732292fd49
EvanCallejo <109547139+EvanCallejo@users.noreply.github.com>
2025-01-26 10:55:33 -0800

    Updated Database files via Scaffold command. Updated port number in example Blazor solution.

--- Common/Models/Data/CallejoSystemDbContext.cs
+++ Common/Models/Data/CallejoSystemDbContext.cs
@@ -28,7 +28,8 @@ public partial class CallejoSystemDbContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DataContext");
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\;Database=Callejo_System_DB;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
