a2db472b714e808689747a74e03eb54cf17b2339
jdelemos <92290824+jdelemos@users.noreply.github.com>
2025-01-26 11:46:05 -0800

    Logic added for user/admin direction.

---
+++ Common/View/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.View
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // We'll store FkRole as an int here
+        public int Role { get; set; }
+    }
+}
+
