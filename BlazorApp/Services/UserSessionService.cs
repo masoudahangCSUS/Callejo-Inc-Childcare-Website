203e0dfafb57cd8ab58699a881c853bb87951da2
masoudahang <masoudahang@csus.edu>
2025-01-28 11:14:26 -0800

    Fixed the User routing issue, now if the FkRole equals to 1 then once the user logs in, they will see the parentHomepage and the parentNavMenu.

--- BlazorApp/Services/UserSessionService.cs
+++ BlazorApp/Services/UserSessionService.cs
@@ -1,9 +1,15 @@
 ﻿public class UserSessionService
 {
     public bool UserIsLoggedIn { get; private set; }
+    public int? UserRole { get; private set; } // Store the user role
 
     public void SetUserLoggedIn(bool isLoggedIn)
     {
         UserIsLoggedIn = isLoggedIn;
+    }
+    public void SetUser(bool isLoggedIn, int? role = null)
+    {
+        UserIsLoggedIn = isLoggedIn;
+        UserRole = role;
     }
 }
