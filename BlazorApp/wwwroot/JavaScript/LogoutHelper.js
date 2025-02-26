a2991e71d7decdce5fcac40b2c138469c8a88acb
Angel-Cal <125705151+Angel-Cal@users.noreply.github.com>
2025-02-25 21:02:40 -0800

    Adding in Cookie-Authorization
    -Authorization through cookies now possible and fully functional
    
    -Made Cookies persistent for a better user experience, but this has created some issues. These are mostly resolved, but I will not merge to main yet

--- BlazorApp/wwwroot/JavaScript/LogoutHelper.js
+++ BlazorApp/wwwroot/JavaScript/LogoutHelper.js
@@ -1,10 +1,11 @@
-﻿async function logoutApi() {
+﻿// wwwroot/js/logoutHelper.js
+window.logoutApi = async function () {
     const response = await fetch('https://localhost:7139/api/admin/logout', {
         method: 'POST',
-        credentials: 'include', // ensure browser cookies are sent and received
+        credentials: 'include', // ensure cookies are sent/received
         headers: {
             'Content-Type': 'application/json'
         }
     });
     return response.json();
-}
+};
