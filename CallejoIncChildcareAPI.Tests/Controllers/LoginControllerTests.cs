2476850378356366ee29a2f5b0e69b051768bd7b
EvanCallejo <109547139+EvanCallejo@users.noreply.github.com>
2025-04-29 19:07:15 -0700

    Creating Test case classes

---
+++ CallejoIncChildcareAPI.Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallejoIncChildcareAPI.Tests.Controllers
+{
+    internal class CustomerControllerTests
+    {
+    }
+}
